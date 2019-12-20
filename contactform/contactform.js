faea0f3cb8ed18f760cd30c393d7ab6ff9f41692
danielvornctl <danielvorn@gmail.com>
2019-12-19 23:12:18 -0600

    testing out formspree

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -3,6 +3,7 @@ jQuery(document).ready(function($) {
 
   //Contact
   $('form.contactForm').submit(function() {
+    console.log("hello. i work")
     var f = $(this).find('.form-group'),
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
@@ -92,6 +93,7 @@ jQuery(document).ready(function($) {
     else var str = $(this).serialize();
     var action = $(this).attr('action');
     if( ! action ) {
+      console.log('got this far!')
       action = 'contactform/contactform.php';
     }
     $.ajax({
--- contactform/contactform.php
+++ contactform/contactform.php
@@ -8,10 +8,10 @@
 /***************** Configuration *****************/
 
   // Replace with your real receiving email address
-  $contact_email_to = "danielvorn@gmail";
+  $contact_email_to = "contact@example.com";
 
   // Title prefixes
-  $subject_title = "Contact Form Message:";
+  $subject_title = "Contat Form Message:";
   $name_title = "Name:";
   $email_title = "Email:";
   $message_title = "Message:";
