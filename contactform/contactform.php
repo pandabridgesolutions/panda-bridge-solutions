9aaa81a62032519221fc6496bbbdc8c734947291
danielvornctl <danielvorn@gmail.com>
2019-12-19 20:35:40 -0600

    did stuff

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -8,7 +8,7 @@
 /***************** Configuration *****************/
 
   // Replace with your real receiving email address
-  $contact_email_to = "contact@example.com";
+  $contact_email_to = "danielvorn@gmail.com";
 
   // Title prefixes
   $subject_title = "Contat Form Message:";
